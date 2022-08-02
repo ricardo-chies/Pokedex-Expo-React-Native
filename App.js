7d2a502cf71fe4e382d73e6efffd30b9622a2144
Ricardo chies <ricardochies5@gmail.com>
2022-08-02 20:13:08 -0300

    Implementação AppLoading

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar, SafeAreaView, View } from 'react-native';
 import React from 'react';
+import AppLoading from 'expo-app-loading';
 import { useFonts, 
   Montserrat_400Regular,
   Montserrat_700Bold } from "@expo-google-fonts/montserrat";
@@ -14,7 +15,7 @@ const [fontCarregada] = useFonts ({
 });
 
 if(!fontCarregada) {
-  return <View/>
+  return <AppLoading />
 }
 
   return (
